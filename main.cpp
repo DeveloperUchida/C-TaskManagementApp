103d2d7ebdd3cdc6f933234a1571a7c78bd445d2
DeveloperUchida <kouei28poiou@icloud.com>
2024-04-29 18:05:07 +0900

    DataUpdate 2024/04/29

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 
 using namespace std;
 
-//タスクを表すクラス
-class Task{
-    private:
-        string title;
-        string description;
-        bool completed;
+// タスクを表すクラス
+class Task
+{
+private:
+    string title;
+    string description;
+    bool completed;
+
+        public:
+        Task(string t, string desc) : title(t), description(desc), completed(false) {}
+
+        void markAsCompleted(){
+            completed = true;
+        }
+        
 };
