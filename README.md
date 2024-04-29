40758e3d32a5dd683847f39d2c0647826f5c0a2b
DeveloperUchida <kouei28poiou@icloud.com>
2024-04-29 17:59:21 +0900

    DataUpdate 2024/04/29

---
+++ README.md
@@ -0,0 +1,6 @@
+# C-TaskManagementApp
+# DevelopmentEnviloment
+- MacOS Sonoma 14.4.1
+- Microsoft VisualStudioCode
+- DevelopmentLang C++
+&copy; 2024DeveloperUchida
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//タスクを表すクラス
+class Task{
+    private:
+        string title;
+        string description;
+        bool completed;
+};
